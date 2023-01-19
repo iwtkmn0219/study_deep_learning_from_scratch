19329c7546f58b0ed073c8d19a47e953fa051af5
iwtkmn0219 <iwtkmn0219@naver.com>
2023-01-20 05:13:25 +0900

    :sparkles: Check step_function is Well
    Check with matplotlib

--- 3.neural_network.py
+++ 3.neural_network.py
@@ -1,6 +1,14 @@
 import numpy as np
+import matplotlib.pylab as plt
 
 
 def step_function(x: np.ndarray) -> np.ndarray:
     y = x > 0
     return y.astype(np.int)
+
+
+x = np.arange(-5, 5, 0.1)
+y = step_function(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)
+plt.show()
