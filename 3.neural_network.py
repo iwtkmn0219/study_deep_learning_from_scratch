76ad3bf3e259860ad173482c81ef7029577f52d6
iwtkmn0219 <iwtkmn0219@naver.com>
2023-01-20 06:05:08 +0900

    :art: Use Dictionary for Weight and Bias

--- 3.neural_network.py
+++ 3.neural_network.py
@@ -24,17 +24,23 @@ def identity_function(x: np.ndarray) -> np.ndarray:
 # plt.ylim(-0.1, 2)
 # plt.show()
 
+network = {
+    "W1": np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]]),
+    "B1": np.array([0.1, 0.2, 0.3]),
+    "W2": np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]]),
+    "B2": np.array([0.1, 0.2]),
+    "W3": np.array([[0.1, 0.3], [0.2, 0.4]]),
+    "B3": np.array([0.1, 0.2]),
+}
+W1, W2, W3 = network["W1"], network["W2"], network["W3"]
+B1, B2, B3 = network["B1"], network["B2"], network["B3"]
+
+# 3-layer neural network
 X = np.array([1.0, 0.5])
-W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
-B1 = np.array([0.1, 0.2, 0.3])
 A1 = np.dot(X, W1) + B1
 Z1 = sigmoid(A1)
-W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
-B2 = np.array([0.1, 0.2])
 A2 = np.dot(Z1, W2) + B2
 Z2 = sigmoid(A2)
-W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
-B3 = np.array([0.1, 0.2])
 A3 = np.dot(Z2, W3) + B3
 Y = identity_function(A3)
 print(Y)
