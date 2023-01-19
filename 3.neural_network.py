865ed75ce8c607a34a4f6aa3c0be54c2bfbc5f97
iwtkmn0219 <iwtkmn0219@naver.com>
2023-01-20 05:59:09 +0900

    :sparkles: Make 3-layer Neural Network

--- 3.neural_network.py
+++ 3.neural_network.py
@@ -15,7 +15,26 @@ def relu(x: np.ndarray) -> np.ndarray:
     return np.maximum(0, x)
 
 
-x = np.arange(-5, 5, 0.1)
-plt.plot(x, step_function(x), x, sigmoid(x), x, relu(x))
-plt.ylim(-0.1, 2)
-plt.show()
+def identity_function(x: np.ndarray) -> np.ndarray:
+    return x
+
+
+# x = np.arange(-5, 5, 0.1)
+# plt.plot(x, step_function(x), x, sigmoid(x), x, relu(x))
+# plt.ylim(-0.1, 2)
+# plt.show()
+
+X = np.array([1.0, 0.5])
+W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+B1 = np.array([0.1, 0.2, 0.3])
+A1 = np.dot(X, W1) + B1
+Z1 = sigmoid(A1)
+W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+B2 = np.array([0.1, 0.2])
+A2 = np.dot(Z1, W2) + B2
+Z2 = sigmoid(A2)
+W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
+B3 = np.array([0.1, 0.2])
+A3 = np.dot(Z2, W3) + B3
+Y = identity_function(A3)
+print(Y)
