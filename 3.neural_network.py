102a3b5b02d0a59aef014f214e4f60447cca19bb
iwtkmn0219 <iwtkmn0219@naver.com>
2023-01-20 06:34:28 +0900

    :art: Convert Code to Function

--- 3.neural_network.py
+++ 3.neural_network.py
@@ -19,28 +19,34 @@ def identity_function(x: np.ndarray) -> np.ndarray:
     return x
 
 
-# x = np.arange(-5, 5, 0.1)
-# plt.plot(x, step_function(x), x, sigmoid(x), x, relu(x))
-# plt.ylim(-0.1, 2)
-# plt.show()
+def init_network() -> dict:
+    network = {
+        "W1": np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]]),
+        "B1": np.array([0.1, 0.2, 0.3]),
+        "W2": np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]]),
+        "B2": np.array([0.1, 0.2]),
+        "W3": np.array([[0.1, 0.3], [0.2, 0.4]]),
+        "B3": np.array([0.1, 0.2]),
+    }
 
-network = {
-    "W1": np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]]),
-    "B1": np.array([0.1, 0.2, 0.3]),
-    "W2": np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]]),
-    "B2": np.array([0.1, 0.2]),
-    "W3": np.array([[0.1, 0.3], [0.2, 0.4]]),
-    "B3": np.array([0.1, 0.2]),
-}
-W1, W2, W3 = network["W1"], network["W2"], network["W3"]
-B1, B2, B3 = network["B1"], network["B2"], network["B3"]
+    return network
 
-# 3-layer neural network
+
+def forward(network: dict, x: np.ndarray) -> np.ndarray:
+    W1, W2, W3 = network["W1"], network["W2"], network["W3"]
+    B1, B2, B3 = network["B1"], network["B2"], network["B3"]
+
+    A1 = np.dot(X, W1) + B1
+    Z1 = sigmoid(A1)
+    A2 = np.dot(Z1, W2) + B2
+    Z2 = sigmoid(A2)
+    A3 = np.dot(Z2, W3) + B3
+    Y = identity_function(A3)
+
+    return Y
+
+
+network = init_network()
 X = np.array([1.0, 0.5])
-A1 = np.dot(X, W1) + B1
-Z1 = sigmoid(A1)
-A2 = np.dot(Z1, W2) + B2
-Z2 = sigmoid(A2)
-A3 = np.dot(Z2, W3) + B3
-Y = identity_function(A3)
+Y = forward(network, X)
 print(Y)
