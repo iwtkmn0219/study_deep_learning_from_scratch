d1231e45cd13b8bbaf355948e1a71b6f29d1912c
iwtkmn0219 <iwtkmn0219@naver.com>
2023-01-20 05:07:36 +0900

    :zap: Use ndarray in step_function Function

--- 3.neural_network.py
+++ 3.neural_network.py
@@ -1,5 +1,6 @@
-def step_function(x):
-    if x > 0:
-        return 1
-    else:
-        return 0
+import numpy as np
+
+
+def step_function(x: np.ndarray) -> np.ndarray:
+    y = x > 0
+    return y.astype(np.int)
