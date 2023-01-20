cd33acc1f9338908e8fe06dbb0c4d3858be076b5
iwtkmn0219 <iwtkmn0219@naver.com>
2023-01-20 10:33:48 +0900

    ⚡️ Add Batch

--- 03/3.1.handwriting_recognition.py
+++ 03/3.1.handwriting_recognition.py
@@ -42,10 +42,11 @@ def predict(network, x):
 x, t = get_data()
 network = init_network()
 
+batch_size = 100
 accuracy_count = 0
-for i in range(len(x)):
-    y = predict(network, x[i])
-    p = np.argmax(y)
-    if p == t[i]:
-        accuracy_count += 1
+for i in range(0, len(x), batch_size):
+    x_batch = x[i : i + batch_size]
+    y_batch = predict(network, x_batch)
+    p = np.argmax(y_batch, axis=1)
+    accuracy_count += np.sum(p == t[i : i + batch_size])
 print(f"Accuracy: {accuracy_count / len(x)}")
